833079823dd26e791d092a2016e1d3f597dcc7d4
Tonylex18 <anthonyagada2000@gmail.com>
2023-02-11 14:24:17 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+$(".play").on("click", function () {
+  owl.trigger("play.owl.autoplay", [5000]);
+});
+$(".stop").on("click", function () {
+  owl.trigger("stop.owl.autoplay");
+});
